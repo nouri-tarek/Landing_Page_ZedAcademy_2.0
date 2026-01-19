eaf74368e431cba0c734dab9a1cc9ea9c7876b7a
nouri-tarek <nouritarek2001@gmail.com>
2026-01-19 02:35:51 +0100

    Starting the project
    Create an Astro project, create a repository on GitHub, and create the Footer module.

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
+});
